da7717cb3f64e04641abcd143a4d612b8a15d859
iskonplus <58971909+iskonplus@users.noreply.github.com>
2023-05-08 12:12:20 +0300

    feat: add solution 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,19 @@
+const fs = require("fs");
+const path = require("path");
+const routeNewFolder = path.join(__dirname, "files-copy");
+const routefiles = path.join(__dirname, "files");
+
+fs.mkdir(routeNewFolder, (err)=>{});
+
+fs.promises.readdir(routefiles, { withFileTypes: true })
+    .then(files => files.forEach(el => {
+            
+        let routeEl = path.join(__dirname, "files", el.name);
+        let routeNewFile = path.join(__dirname, "files-copy", el.name);
+   
+        fs.readFile(routeEl, (err, content) => {
+            err || fs.writeFile(routeNewFile, content.toString(), err => {});
+        })
+
+    }));
 
