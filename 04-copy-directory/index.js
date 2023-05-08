ebd52067856ed8be4a337e2c36c666f22e85e273
iskonplus <58971909+iskonplus@users.noreply.github.com>
2023-05-08 18:03:07 +0300

    feat: add delete deleted file

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,19 +1,25 @@
 const fs = require("fs");
 const path = require("path");
-const routeNewFolder = path.join(__dirname, "files-copy");
 const routefiles = path.join(__dirname, "files");
+const routeNewFolder = path.join(__dirname, "files-copy");
 
-fs.mkdir(routeNewFolder, (err)=>{});
+fs.mkdir(routeNewFolder, (err) => {});
 
-fs.promises.readdir(routefiles, { withFileTypes: true })
-    .then(files => files.forEach(el => {
-            
-        let routeEl = path.join(__dirname, "files", el.name);
-        let routeNewFile = path.join(__dirname, "files-copy", el.name);
-   
-        fs.readFile(routeEl, (err, content) => {
-            err || fs.writeFile(routeNewFile, content.toString(), err => {});
-        })
+fs.promises
+  .readdir(routeNewFolder, { withFileTypes: true })
+  .then((files) =>
+    files.forEach((el) => {
+      fs.unlink(path.join(__dirname, "files-copy", el.name), (err) => {});
+    })
+  );
 
-    }));
+fs.promises.readdir(routefiles, { withFileTypes: true }).then((files) =>
+  files.forEach((el) => {
+    let routeEl = path.join(__dirname, "files", el.name);
+    let routeNewFile = path.join(__dirname, "files-copy", el.name);
 
+    fs.readFile(routeEl, (err, content) => {
+      err || fs.writeFile(routeNewFile, content.toString(), (err) => {});
+    });
+  })
+);
